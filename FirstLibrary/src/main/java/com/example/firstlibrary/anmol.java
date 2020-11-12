ab376eccd46e7cc5713d55312d51afcc55a47892
Anmol <anandanmol1010@gmail.com>
2020-11-13 00:32:13 +0530

    Initial Commit

---
+++ FirstLibrary/src/main/java/com/example/firstlibrary/anmol.java
@@ -0,0 +1,10 @@
+package com.example.firstlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class anmol {
+    public static void s(Context c, String message){
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+    }
+}
